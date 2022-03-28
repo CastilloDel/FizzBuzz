2909846923877466cc97afbc8f3425ab58cce23c
CastilloDel <delcastillodelarosadaniel@gmail.com>
2022-03-28 14:01:30 +0100

    Haskell: returns the correct number string

--- haskell/test/FizzBuzzTest.hs
+++ haskell/test/FizzBuzzTest.hs
@@ -8,5 +8,6 @@ main = hspec fizzBuzzTests
 
 fizzBuzzTests :: Spec
 fizzBuzzTests = describe "Fizzbuzz behaviour" $ do
-  it "Should return the correct number" $
+  it "Should return the correct number" $ do
     fizzBuzz 1 `shouldBe` "1"
+    fizzBuzz 4 `shouldBe` "4"
