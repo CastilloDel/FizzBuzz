ced1906a59b7d005e909637d61d68b14b366b2a4
CastilloDel <delcastillodelarosadaniel@gmail.com>
2022-03-28 14:05:54 +0100

    Haskell: Add Buzz

--- haskell/src/FizzBuzz.hs
+++ haskell/src/FizzBuzz.hs
@@ -3,4 +3,5 @@ module FizzBuzz where
 fizzBuzz :: Int -> String
 fizzBuzz n
   | n `mod` 3 == 0 = "Fizz"
+  | n `mod` 5 == 0 = "Buzz"
   | otherwise = show n
--- haskell/test/FizzBuzzTest.hs
+++ haskell/test/FizzBuzzTest.hs
@@ -14,3 +14,6 @@ fizzBuzzTests = describe "Fizzbuzz behaviour" $ do
   it "Should return Fizz if the number is multiple of 3" $ do
     fizzBuzz 3 `shouldBe` "Fizz"
     fizzBuzz 21 `shouldBe` "Fizz"
+  it "Should return Buzz if the number is multiple of 5" $ do
+    fizzBuzz 5 `shouldBe` "Buzz"
+    fizzBuzz 25 `shouldBe` "Buzz"
