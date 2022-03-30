e24bb8685f8f179841c73faaf71a537be906e5db
CastilloDel <delcastillodelarosadaniel@gmail.com>
2022-03-30 14:51:22 +0100

    Python: Refactor tests

--- python/fizzbuzz_test.py
+++ python/fizzbuzz_test.py
@@ -3,26 +3,26 @@ import unittest
 from src.fizzbuzz import FizzBuzz
 
 class FizzBuzzTests(unittest.TestCase):
+  def __init__(self, *args):
+    super().__init__(*args)
+    self.fizzbuzz = FizzBuzz()
+
   def test_returns_number_string(self):
-    fizzbuzz = FizzBuzz()
-    self.assertEqual(fizzbuzz.to_fizzbuzz(1), '1')
-    self.assertEqual(fizzbuzz.to_fizzbuzz(4), '4')
+    self.assertEqual(self.fizzbuzz.to_fizzbuzz(1), '1')
+    self.assertEqual(self.fizzbuzz.to_fizzbuzz(4), '4')
 
   def test_returns_fizz_if_multiple_of_3(self):
-    fizzbuzz = FizzBuzz()
-    self.assertEqual(fizzbuzz.to_fizzbuzz(3), 'Fizz')
-    self.assertEqual(fizzbuzz.to_fizzbuzz(18), 'Fizz')
-    self.assertEqual(fizzbuzz.to_fizzbuzz(363), 'Fizz')
+    self.assertEqual(self.fizzbuzz.to_fizzbuzz(3), 'Fizz')
+    self.assertEqual(self.fizzbuzz.to_fizzbuzz(18), 'Fizz')
+    self.assertEqual(self.fizzbuzz.to_fizzbuzz(363), 'Fizz')
 
   def test_returns_buzz_if_multiple_of_5(self):
-    fizzbuzz = FizzBuzz()
-    self.assertEqual(fizzbuzz.to_fizzbuzz(5), 'Buzz')
-    self.assertEqual(fizzbuzz.to_fizzbuzz(20), 'Buzz')
-    self.assertEqual(fizzbuzz.to_fizzbuzz(425), 'Buzz')
+    self.assertEqual(self.fizzbuzz.to_fizzbuzz(5), 'Buzz')
+    self.assertEqual(self.fizzbuzz.to_fizzbuzz(20), 'Buzz')
+    self.assertEqual(self.fizzbuzz.to_fizzbuzz(425), 'Buzz')
    
   def test_returns_fizzbuzz_if_multiple_of_3_and_5(self):
-    fizzbuzz = FizzBuzz()
-    self.assertEqual(fizzbuzz.to_fizzbuzz(15), 'FizzBuzz')
-    self.assertEqual(fizzbuzz.to_fizzbuzz(720), 'FizzBuzz')
+    self.assertEqual(self.fizzbuzz.to_fizzbuzz(15), 'FizzBuzz')
+    self.assertEqual(self.fizzbuzz.to_fizzbuzz(720), 'FizzBuzz')
 
 unittest.main()
