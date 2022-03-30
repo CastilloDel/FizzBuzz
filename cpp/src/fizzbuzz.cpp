94590107635c4ab4cbf46a5c4ce3133b30b2f018
CastilloDel <delcastillodelarosadaniel@gmail.com>
2022-03-30 11:54:38 +0100

    C++: add Buzz if number is multiple of 5

--- cpp/src/fizzbuzz.cpp
+++ cpp/src/fizzbuzz.cpp
@@ -6,5 +6,8 @@ string FizzBuzz::get(int number) {
     if (number % 3 == 0) {
         return "Fizz";
     }
+    if (number % 5 == 0) {
+        return "Buzz";
+    }
     return to_string(number);
 }
--- cpp/test/fizzbuzz_test.cpp
+++ cpp/test/fizzbuzz_test.cpp
@@ -12,3 +12,9 @@ TEST(FizzBuzz, ReturnsFizzIfNumberIsMultipleOf3) {
   EXPECT_EQ(fizzbuzz.get(3), "Fizz");
   EXPECT_EQ(fizzbuzz.get(18), "Fizz");
 }
+
+TEST(FizzBuzz, ReturnsBuzzIfNumberIsMultipleOf5) {
+  FizzBuzz fizzbuzz;
+  EXPECT_EQ(fizzbuzz.get(5), "Buzz");
+  EXPECT_EQ(fizzbuzz.get(20), "Buzz");
+}
