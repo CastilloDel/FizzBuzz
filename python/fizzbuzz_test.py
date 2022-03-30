56d9ba8ee86aae89c1bcd8d051de56337f47b260
CastilloDel <delcastillodelarosadaniel@gmail.com>
2022-03-30 14:05:37 +0100

    Python: add Fizz

--- python/fizzbuzz_test.py
+++ python/fizzbuzz_test.py
@@ -7,6 +7,11 @@ class FizzBuzzTests(unittest.TestCase):
     fizzbuzz = FizzBuzz()
     self.assertEqual(fizzbuzz.to_fizzbuzz(1), '1')
     self.assertEqual(fizzbuzz.to_fizzbuzz(4), '4')
+
+  def test_returns_fizz_if_multiple_of_3(self):
+    fizzbuzz = FizzBuzz()
+    self.assertEqual(fizzbuzz.to_fizzbuzz(3), 'Fizz')
+    self.assertEqual(fizzbuzz.to_fizzbuzz(18), 'Fizz')
    
 
 unittest.main()
--- python/src/fizzbuzz.py
+++ python/src/fizzbuzz.py
@@ -1,3 +1,11 @@
 class FizzBuzz():
   def to_fizzbuzz(self, number: int):
+    copy = number
+    digit_sum = 0
+    while copy > 0:
+      digit_sum += copy % 10
+      copy //= 10
+    if digit_sum in [3, 6, 9]:
+      return 'Fizz'
     return str(number)
+    
