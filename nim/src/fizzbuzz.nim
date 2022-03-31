813e8f6bc9f28c94587e02f06d2ace9758232275
CastilloDel <delcastillodelarosadaniel@gmail.com>
2022-03-31 12:46:32 +0100

    Nim: Add Buzz

--- nim/src/fizzbuzz.nim
+++ nim/src/fizzbuzz.nim
@@ -1,5 +1,7 @@
 
-proc fizzbuzz*(number: int): string =
+proc fizzbuzz*(number: int): string {.compileTime.} =
   if number mod 3 == 0:
     return "Fizz"
+  if number mod 5 == 0:
+    return "Buzz"
   $number
--- nim/tests/test_fizzbuzz.nim
+++ nim/tests/test_fizzbuzz.nim
@@ -6,3 +6,5 @@ static: (fizzbuzz(1) == "1" and fizzbuzz(53) == "53")
 static: (fizzbuzz(3) == "Fizz" and fizzbuzz(99) == "Fizz")
     .assert "Returns Fizz if number is a multiple of 3"
 
+static: (fizzbuzz(5) == "Buzz" and fizzbuzz(100) == "Buzz")
+    .assert "Returns Buzz if number is a multiple of 5"
