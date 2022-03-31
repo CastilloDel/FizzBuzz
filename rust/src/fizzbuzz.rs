e5cbfb5bf7970e2d5d55f3ba44c5478b959e0f1d
CastilloDel <delcastillodelarosadaniel@gmail.com>
2022-03-31 09:38:35 +0100

    Rust: Add Buzz

--- rust/src/fizzbuzz.rs
+++ rust/src/fizzbuzz.rs
@@ -1,6 +1,8 @@
 fn get_fizzbuzz_equivalent(number: usize) -> String {
     if number % 3 == 0 {
         "Fizz".to_string()
+    } else if number % 5 == 0 {
+        "Buzz".to_string()
     } else {
         number.to_string()
     }
@@ -19,5 +21,11 @@ mod tests {
     fn should_return_fizz_if_multiple_of_3() {
         assert_eq!(get_fizzbuzz_equivalent(3), "Fizz");
         assert_eq!(get_fizzbuzz_equivalent(93), "Fizz");
+    }
+
+    #[test]
+    fn should_return_buzz_if_multiple_of_5() {
+        assert_eq!(get_fizzbuzz_equivalent(5), "Buzz");
+        assert_eq!(get_fizzbuzz_equivalent(95), "Buzz");
     }
 }
