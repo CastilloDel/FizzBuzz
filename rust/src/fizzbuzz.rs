a55d0e77e957e30757585e4deb6404a3fdcc0138
CastilloDel <delcastillodelarosadaniel@gmail.com>
2022-03-31 09:32:31 +0100

    Rust: Return the string version of the number

--- rust/src/fizzbuzz.rs
+++ rust/src/fizzbuzz.rs
@@ -1,9 +1,13 @@
-struct FizzBuzz {}
+fn get_fizzbuzz_equivalent(number: usize) -> String {
+    number.to_string()
+}
 
 #[cfg(test)]
 mod tests {
+    use super::get_fizzbuzz_equivalent;
     #[test]
     fn test() {
-        assert!(true);
+        assert_eq!(get_fizzbuzz_equivalent(1), "1");
+        assert_eq!(get_fizzbuzz_equivalent(124), "124");
     }
 }
