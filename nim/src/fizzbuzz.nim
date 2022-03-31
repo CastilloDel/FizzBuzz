60eb46bdc7e8ca95d18f208fe71e998e91a9a151
CastilloDel <delcastillodelarosadaniel@gmail.com>
2022-03-31 12:42:23 +0100

    Nim: Add fizz

--- nim/src/fizzbuzz.nim
+++ nim/src/fizzbuzz.nim
@@ -1,3 +1,5 @@
 
 proc fizzbuzz*(number: int): string =
+  if number mod 3 == 0:
+    return "Fizz"
   $number
--- nim/tests/test_fizzbuzz.nim
+++ nim/tests/test_fizzbuzz.nim
@@ -3,4 +3,6 @@ import ../src/fizzbuzz
 static: (fizzbuzz(1) == "1" and fizzbuzz(53) == "53")
     .assert "Doesn't return the correct number string"
 
+static: (fizzbuzz(3) == "Fizz" and fizzbuzz(99) == "Fizz")
+    .assert "Returns Fizz if number is a multiple of 3"
 
