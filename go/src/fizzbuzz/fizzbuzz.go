09d56818cd90512539adae5ae8f8fa695473d209
CastilloDel <delcastillodelarosadaniel@gmail.com>
2022-04-01 13:33:48 +0100

    Go: initial setup

---
+++ go/src/fizzbuzz/fizzbuzz.go
@@ -0,0 +1,3 @@
+package fizzbuzz
+
+type FizzBuzz struct{}
---
+++ go/src/fizzbuzz/fizzbuzz_test.go
@@ -0,0 +1,11 @@
+package fizzbuzz
+
+import "testing"
+
+func TestFizzBuzz(t *testing.T) {
+	got := FizzBuzz{}
+
+	if true {
+		t.Errorf("Hello %q", got)
+	}
+}
