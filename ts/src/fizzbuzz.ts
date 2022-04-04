3d1562a026ac3d0d14cb446303cb935da61a3a8d
CastilloDel <delcastillodelarosadaniel@gmail.com>
2022-04-04 13:30:00 +0100

    TS: Add end to the iterator

--- ts/src/fizzbuzz.ts
+++ ts/src/fizzbuzz.ts
@@ -1,5 +1,5 @@
-export function* getFizzBuzzIterator() {
-  for (let i = 1; i <= 100; i++) {
+export function* getFizzBuzzIterator(limit: number = 100) {
+  for (let i = 1; i <= limit; i++) {
     yield i.toString();
   }
 }
--- ts/test/fizzbuzz.test.ts
+++ ts/test/fizzbuzz.test.ts
@@ -1,7 +1,22 @@
 import { getFizzBuzzIterator } from "../src/fizzbuzz";
 
-test("", () => {
-  const iterator = getFizzBuzzIterator();
-  expect(iterator.next().value).toBe("1");
-  expect(iterator.next().value).toBe("2");
+describe("Fizzbuzz", () => {
+  test("Should count", () => {
+    const iterator = getFizzBuzzIterator();
+
+    expect(iterator.next().value).toBe("1");
+    expect(iterator.next().value).toBe("2");
+  });
+
+  test("Should be possible to set an end for the iterator", () => {
+    const limit = 23;
+    const iterator = getFizzBuzzIterator(limit);
+
+    let count = 0;
+    for (const element of iterator) {
+      count++;
+    }
+
+    expect(count).toBe(limit);
+  });
 });
