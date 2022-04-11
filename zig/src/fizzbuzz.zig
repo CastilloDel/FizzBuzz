5672be093075b4dc9a0c776275c2b37683e72a29
CastilloDel <delcastillodelarosadaniel@gmail.com>
2022-04-11 11:23:26 +0100

    Zig: add FizzBuzz

--- zig/src/fizzbuzz.zig
+++ zig/src/fizzbuzz.zig
@@ -3,10 +3,11 @@ var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 const allocator = gpa.allocator();
 
 pub fn get_fizzbuzz(comptime n: usize) []const u8 {
-    if (n % 3 == 0) {
+    if (n % 15 == 0) {
+        return "FizzBuzz";
+    } else if (n % 3 == 0) {
         return "Fizz";
-    }
-    if (n % 5 == 0) {
+    } else if (n % 5 == 0) {
         return "Buzz";
     }
     return std.fmt.comptimePrint("{}", .{n});
@@ -27,4 +28,9 @@ test "Should return Fizz is the number is multiple of 3" {
 test "Should return Buzz is the number is multiple of 5" {
     try testing.expectEqualSlices(u8, get_fizzbuzz(5), "Buzz");
     try testing.expectEqualSlices(u8, get_fizzbuzz(5 * 34), "Buzz");
+}
+
+test "Should return FizzBuzz is the number is multiple of 3 and 5" {
+    try testing.expectEqualSlices(u8, get_fizzbuzz(3 * 5), "FizzBuzz");
+    try testing.expectEqualSlices(u8, get_fizzbuzz(3 * 5 * 34), "FizzBuzz");
 }
