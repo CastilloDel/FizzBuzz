9c83e08ad879cae9dcf6c2ca804132932d39858d
CastilloDel <delcastillodelarosadaniel@gmail.com>
2022-04-11 11:15:08 +0100

    Zig: Add Fizz

--- zig/src/fizzbuzz.zig
+++ zig/src/fizzbuzz.zig
@@ -3,6 +3,9 @@ var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 const allocator = gpa.allocator();
 
 pub fn get_fizzbuzz(comptime n: usize) []const u8 {
+    if (n % 3 == 0) {
+        return "Fizz";
+    }
     return std.fmt.comptimePrint("{}", .{n});
 }
 
@@ -11,4 +14,9 @@ const testing = std.testing;
 test "Should return the passed number in a string" {
     try testing.expectEqualSlices(u8, get_fizzbuzz(1), "1");
     try testing.expectEqualSlices(u8, get_fizzbuzz(17), "17");
+}
+
+test "Should return Fizz is the number is multiple of 3" {
+    try testing.expectEqualSlices(u8, get_fizzbuzz(3), "Fizz");
+    try testing.expectEqualSlices(u8, get_fizzbuzz(3 * 34), "Fizz");
 }
