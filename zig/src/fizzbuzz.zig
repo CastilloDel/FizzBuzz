314056ef930a544a5236e32445971b607337ce4d
CastilloDel <delcastillodelarosadaniel@gmail.com>
2022-04-11 11:16:45 +0100

    Zig: add Buzz

--- zig/src/fizzbuzz.zig
+++ zig/src/fizzbuzz.zig
@@ -6,6 +6,9 @@ pub fn get_fizzbuzz(comptime n: usize) []const u8 {
     if (n % 3 == 0) {
         return "Fizz";
     }
+    if (n % 5 == 0) {
+        return "Buzz";
+    }
     return std.fmt.comptimePrint("{}", .{n});
 }
 
@@ -19,4 +22,9 @@ test "Should return the passed number in a string" {
 test "Should return Fizz is the number is multiple of 3" {
     try testing.expectEqualSlices(u8, get_fizzbuzz(3), "Fizz");
     try testing.expectEqualSlices(u8, get_fizzbuzz(3 * 34), "Fizz");
+}
+
+test "Should return Buzz is the number is multiple of 5" {
+    try testing.expectEqualSlices(u8, get_fizzbuzz(5), "Buzz");
+    try testing.expectEqualSlices(u8, get_fizzbuzz(5 * 34), "Buzz");
 }
