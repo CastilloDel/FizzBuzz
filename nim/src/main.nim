226d7854d7233684eb633ba07da6f0ba195fb1a9
CastilloDel <delcastillodelarosadaniel@gmail.com>
2022-04-12 13:21:39 +0100

    Nim: Fix the program starting on zero

--- nim/src/main.nim
+++ nim/src/main.nim
@@ -2,6 +2,6 @@ import std/sequtils
 import std/sugar
 import fizzbuzz
 
-const numbers = toSeq(0..100).map(x => fizzbuzz(x))
+const numbers = toSeq(1..100).map(x => fizzbuzz(x))
 for num in numbers:
     echo num
